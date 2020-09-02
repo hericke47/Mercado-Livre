c1c4dcedf502444c4a9334859b43339b605fd5df
Herick Exterkoetter <herickherick47@gmail.com>
2020-09-01 22:34:57 -0300

    esqueleto de Product

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,13 +1,76 @@
 import React from 'react';
 
-import { Container } from './styles';
+import tshirtImage from '../../assets/tshirt.png';
+
+import { Container, Row, Panel, Column, Gallery, Section, Description } from './styles';
 
 const Product: React.FC = () => {
   return (
     <Container>
-      <h1>Product</h1>
+      <Row>
+        <a href="#">Compartilhar</a>
+        <a href="#">Vender um igual</a>
+      </Row>
+
+      <Panel>
+        <Column>
+          <Gallery>
+            <img alt="T-Shirt" src={tshirtImage} />
+          </Gallery>
+
+          <Info />
+        </Column>
+
+        <Column>
+          {/* <ProductAction />
+          <SellerInfo /> */}
+
+          <WarrantySection />
+          <WarrantySection />
+          <WarrantySection />
+        </Column>
+      </Panel>
     </Container>
   );
 };
+
+const WarrantySection = () => (
+  <Section>
+    <h4>Garantia</h4>
+
+    <div>
+      <span>
+        <p className="title">Compra garantida com o Lorem Ipsum</p>
+        <p className="description">Receba o produto que voce esta esperando ou devolvemos o seu dinheiro</p>
+      </span>
+
+      <span>
+        <p className="title">Garantia do vendedor</p>
+        <p className="description">sem garantia</p>
+      </span>
+    </div>
+  </Section>
+)
+
+const Info = () => (
+  <Description>
+    <h2>Descrição</h2>
+
+    <p>
+    A expressão Lorem ipsum em design gráfico e editoração é um texto padrão em latim utilizado na produção gráfica para preencher os espaços de texto em publicações para testar e ajustar aspectos visuais antes de utilizar conteúdo real. Também é utilizado em catálogos tipográficos, para demonstrar textos e títulos escritos com as fontes.
+    <br />
+    <br />
+    Itens inclusos: <br />
+    - 1x LED <br />
+    - 1x LED <br />
+    - 1x LED <br />
+    - 1x LED <br />
+    - 1x LED <br />
+    <br />
+    A expressão Lorem ipsum em design gráfico e editoração é um texto padrão em latim utilizado na produção gráfica para preencher os espaços de texto em publicações para testar e ajustar aspectos visuais antes de utilizar conteúdo real. Também é utilizado em catálogos tipográficos, para demonstrar textos e títulos escritos com as fontes.
+
+    </p>
+  </Description>
+)
 
 export default Product;
